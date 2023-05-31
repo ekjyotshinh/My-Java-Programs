c90cceba75445a62f579a5b9db3e89e7c4a8e892
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2023-05-31 12:28:06 -0700

    Renamed

--- TodoList.java
+++ TodoList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Todo {
+public class TodoList {
 
     public static void main(String[] args) {
         ArrayList<String> tasks = new ArrayList<>();
